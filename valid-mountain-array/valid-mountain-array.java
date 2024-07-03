7e2a33fb662d2402cf25f40b22cbe25c2b64eb92
HyunwooYi <83715335+HyunwooYi@users.noreply.github.com>
2024-07-04 02:31:24 +0900

    Time: 5 ms, Memory: 45.6 MB - LeetHub

--- valid-mountain-array/valid-mountain-array.java
+++ valid-mountain-array/valid-mountain-array.java
@@ -3,16 +3,15 @@ import java.util.Arrays;
 class Solution {
 
     public boolean validMountainArray(int[] arr) {
-        System.out.println("22222222");
-
-        if (arr.length <3){
+        
+        if (arr.length < 3) {
             return false;
         }
 
         int max = Arrays.stream(arr).max().getAsInt();
 
         // if "last index value is max" or "0th index value is max" -> return false
-        if(arr[arr.length-1] == max || arr[0] == max){
+        if (arr[arr.length - 1] == max || arr[0] == max) {
             return false;
         }
 
@@ -20,20 +19,19 @@ class Solution {
         if (arr[0] > arr[1]) {
             return false;
         }
+        
         int k = 2;
-        for (int i = 1; i<arr.length; i++){
+        for (int i = 1; i < arr.length; i++) {
             if (arr[i] < arr[k]) {
 
             } else {
-//                int t = k+1;
-                for (int s = i; s <arr.length; s++) {
-                    if (arr[s] > arr[s+1]) {
-                        if (s+1 == arr.length-1){
+                for (int s = i; s < arr.length; s++) {
+                    if (arr[s] > arr[s + 1]) {
+                        if (s + 1 == arr.length - 1) {
                             return true;
                         }
                     } else
                         return false;
-//                    t++;
                 }
             }
             k++;
